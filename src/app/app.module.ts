383def927c6140612eb3c19c723a4b589de9df24
sofienargoubi <sofien.argoubi@esprit.tn>
2021-10-03 16:05:04 +0100

    generate component (home,contact,login ..)

---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { FormContactComponent } from './components/form-contact/form-contact.component';
+import { ListContactComponent } from './components/list-contact/list-contact.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'contact', component: ListContactComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListContactComponent,
+    FormContactComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule.forRoot(routes)
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/list-contact/list-contact.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-contact',
+  templateUrl: './list-contact.component.html',
+  styleUrls: ['./list-contact.component.css']
+})
+export class ListContactComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
